77fe0f84ed67b9ca41715fd1c437451e4972c6c1
Mart van den Broek <mart.vandenbroek@triodos.com>
2026-06-23 16:42:34 +0200

    ssdf

--- src/test/java/com/example/demo/MarioControllerTest.java
+++ src/test/java/com/example/demo/MarioControllerTest.java
@@ -19,6 +19,6 @@ class MarioControllerTest {
     void mario_shouldReturnCatchPhrase() throws Exception {
         mockMvc.perform(get("/mario"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("It's-a me, Mario!"));
+                .andExpect(content().string("It's-a me, Maridsdo!"));
     }
 }
